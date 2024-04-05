a3db28661d3c3172aa10bc5fe900505452cc686c
Hamza Elhamdadi <irishlad98@Hamzas-MacBook-Pro.local>
2024-04-05 12:26:12 -0400

    started an uninformed player

---
+++ uninformed_player.py
@@ -0,0 +1,25 @@
+from pypokerengine.players import BasePokerPlayer
+
+class UninformedPlayer(BasePokerPlayer):
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        return valid_actions[1]['action']
+    
+    def receive_game_start_message(self, game_info):
+        pass
+    
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+    
+    def receive_game_update_message(self, new_action, round_state):
+        pass
+    
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+    
+
+def setup_ai():
+    return UninformedPlayer()
