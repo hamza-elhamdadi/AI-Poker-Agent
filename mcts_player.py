07d03fb4c188646829a7b82f4f9803dabaafcc6c
Hamza Elhamdadi <irishlad98@vl965-172-31-192-180.wireless.umass.edu>
2024-05-06 14:33:25 -0400

    readded the MCTS player

---
+++ mcts_player.py
@@ -0,0 +1,69 @@
+from pypokerengine.players import BasePokerPlayer
+from hand_type import handType
+import json
+
+from MCTS.MCTS_win_rate import win_rate
+
+class MCTSPlayer(BasePokerPlayer):
+
+    def estimate_reward(self, action, hole_card, round_state):
+        import random
+        return random.random()
+        # TODO:Use MCTS to build a reward table
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        if round_state['street'] == 'preflop':
+            type_of_hand = handType(hole_card + community_card)
+            if type_of_hand == 2:
+                return 'raise'
+            else:
+                return 'call'
+        else:
+            best_action = 'call'
+            print(hole_card)
+            
+            hole_card.sort()
+            community_card = round_state['community_card']
+            community_values = [c[1] for c in community_card]
+            community_values.sort()
+
+            if round_state['street'] == 'flop':
+                with open(f'MCTS/params/MCTS_params_flop_{"".join(hole_card)}.json', 'r') as file:
+                    win_rates = json.load(file)
+                win_rt = win_rates[''.join(community_values)]
+            else:
+                win_rt = win_rate(hole_card)
+
+            for i in valid_actions:
+                action = i['action']
+                #reward = self.estimate_reward(action, hole_card, round_state)
+                if action == 'fold':
+                    if win_rt <= 0.2:
+                        best_action = action
+                if action == 'call':
+                    if 0.2 < win_rt <= 0.8:
+                        best_action = action
+                if action == 'raise':
+                    if win_rt > 0.8:
+                        best_action = action
+
+            return best_action
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, new_action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+
+def setup_ai():
+    return MCTSPlayer()
